aa14a7249d2027035c94ec3e85e196725a635f1b
Ted Oiler <ladas.thodoris@gmail.com>
2024-06-24 15:41:31 +0300

    orgamized cordex discrete

--- optimizers/cordex_discrete.py
+++ optimizers/cordex_discrete.py
@@ -18,40 +18,55 @@ class CordexDiscrete(BaseOptimizer):
         super().__init__(model)
         self.model = model
 
-    def optimize(self, runs, nx, levels, scalars=0, epochs=1000, final_pass_iter=100):
-        best_design = None
-        best_optimality_value = np.inf
+    def optimize(self, runs, nx, scalars=0, epochs=1000, final_pass_iter=100, levels=None):
+        best_design, best_optimality_value = None, np.inf
 
         for _ in tqdm(range(epochs)):
-            Gamma_, X_ = gen_rand_design_m(runs=runs, f_list=nx, scalars=scalars)
-            model_matrix = Gamma_
-            best_design, best_optimality_value = self._evaluate_levels(model_matrix, levels, runs, nx, scalars,
-                                                                       best_optimality_value)
+            Gamma, _ = gen_rand_design_m(runs=runs, f_list=nx, scalars=scalars)
+            best_design, best_optimality_value = self._cordex_loop(
+                Gamma, runs, nx, scalars, best_optimality_value, best_design, levels)
 
         if final_pass_iter > 0:
             for _ in tqdm(range(final_pass_iter)):
-                best_design, best_optimality_value = self._evaluate_levels(best_design, levels, runs, nx, scalars,
-                                                                           best_optimality_value)
+                best_design, best_optimality_value = self._cordex_loop(
+                    best_design, runs, nx, scalars, best_optimality_value, best_design, levels
+                )
 
         return best_design, np.abs(best_optimality_value)
 
-    def _evaluate_levels(self, model_matrix, levels, runs, nx, scalars, best_optimality_value):
+    def _cordex_loop(self, model_matrix, runs, nx, scalars, best_optimality_value, best_design, levels):
         current_optimality_value = best_optimality_value
 
         for i in range(model_matrix.shape[0]):
             for j in range(model_matrix.shape[1]):
-                if isinstance(self.model, FunctionOnFunctionModel):
-                    objective = lambda x: self.model.compute_objective_input(x, i, j, model_matrix, runs, nx)
-                elif isinstance(self.model, ScalarOnFunctionModel):
-                    objective = lambda x: self.model.compute_objective_input(x, i, j, model_matrix, sum(nx) + scalars)
+                objective = self._get_objective_function(i, j, model_matrix, runs, nx, scalars)
+                best_level, best_obj_value = self._evaluate_objective_levels(
+                    objective, model_matrix[i, j], levels)
 
-                best_lvl_list = [(objective(lvl), lvl) for lvl in levels]
-                best_lvl = min(best_lvl_list, key=lambda x: x[0])[1]
-                model_matrix[i, j] = best_lvl
-                current_optimality_value = objective(model_matrix)
+                model_matrix[i, j] = best_level
+                current_optimality_value = best_obj_value
 
         if 0 <= current_optimality_value < best_optimality_value:
             best_optimality_value = current_optimality_value
             best_design = model_matrix.copy()
 
         return best_design, best_optimality_value
+
+    def _get_objective_function(self, i, j, model_matrix, runs, nx, scalars):
+        if isinstance(self.model, FunctionOnFunctionModel):
+            return lambda x: self.model.compute_objective_input(x, i, j, model_matrix, runs, nx)
+        elif isinstance(self.model, ScalarOnFunctionModel):
+            return lambda x: self.model.compute_objective_input(x, i, j, model_matrix, sum(nx) + scalars)
+        else:
+            raise TypeError("Unsupported model type")
+
+    def _evaluate_objective_levels(self, objective, current_level, levels):
+        best_level = current_level
+        best_obj_value = objective(current_level)
+
+        for level in levels:
+            obj_value = objective(level)
+            if obj_value < best_obj_value:
+                best_level, best_obj_value = level, obj_value
+
+        return best_level, best_obj_value
