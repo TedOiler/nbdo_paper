47d9e208a8e0b411e3cb4ee06c457bf0c5b364f8
Ted Oiler <ladas.thodoris@gmail.com>
2024-06-25 16:18:37 +0300

    working prototype with bug on SoF J matrix calculation

--- mathematical_models/s_on_f.py
+++ mathematical_models/s_on_f.py
@@ -3,14 +3,16 @@ import numpy as np
 import os
 import sys
 
-sys.path.append(os.path.abspath("../utilities"))
-from utilities.matrix_calc import Jcb, calc_basis_matrix
-
 
 class ScalarOnFunctionModel(BaseModel):
-    def __init__(self, Kx, Kb):
+    def __init__(self, Kx, Kb, Kx_family, Kb_family='polynomial', k_degree=None, knots_num=None):
+        self.Kx_family = Kx_family
         self.Kx = Kx
+        self.Kb_family = Kb_family
         self.Kb = Kb
+        self.k_degree = k_degree
+        self.knots_num = knots_num
+
         self.J_cb = self.compute_Jcb()
 
     def compute_objective(self, Model_mat, f_coeffs):
@@ -33,7 +35,15 @@ class ScalarOnFunctionModel(BaseModel):
         return self.compute_objective(Model_mat, f_coeffs)
 
     def compute_Jcb(self):
-        return Jcb(*[calc_basis_matrix(x_basis=x, b_basis=b) for x, b in zip(self.Kx, self.Kb)])
+        if self.Kx_family == 'step':
+            sys.path.append(os.path.abspath("../utilities"))
+            from utilities.matrix_calc import Jcb, calc_basis_matrix
+            return Jcb(*[calc_basis_matrix(x_basis=x, b_basis=b) for x, b in zip(self.Kx, self.Kb)])
+        if self.Kx_family == 'b-spline':
+            sys.path.append(os.path.abspath("../../utilities"))
+            from utilities.J_bsplines_poly import Jcb, calc_basis_matrix
+            return Jcb(*[calc_basis_matrix(x_basis=x, b_basis=b, k=self.k_degree, knots_num=self.knots_num)
+                         for x, b in zip(self.Kx, self.Kb)])
 
     def get_Jcb(self):
         return self.J_cb
