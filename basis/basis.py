6cbaeb5585711e8b31fb114572277050694cc92e
tedoiler <ladas.thodoris@gmail.com>
2024-09-30 14:17:55 +0300

    added plotting for many functional factors

--- basis/basis.py
+++ basis/basis.py
@@ -2,6 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def plot_design(design, basis_list, runs, t_detail=100, style='seaborn-v0_8', sub_x=2, sub_y=2):
+    def split_array_by_columns(arr, col_splits):
+        arrays = []
+        start_col = 0
+        for num_cols in col_splits:
+            end_col = start_col + num_cols
+            sub_array = arr[:, start_col:end_col]
+            arrays.append(sub_array)
+            start_col = end_col
+        return arrays
+
+    plt.style.use(style)
+    t_values = np.linspace(0, 1, t_detail)
+    coefficients_split = split_array_by_columns(design, [basis_list[i].num_basis() for i in range(len(basis_list))])
+    y_values = [basis_list[j].evaluate_combination(coefficients_split[j][i], t_values)
+                for i in range(runs)
+                for j in range(len(coefficients_split))]
+    y_values = np.array(y_values).reshape(runs, len(basis_list), t_detail)
+
+    for i in range(y_values.shape[1]):  # i bases
+        fig, axes = plt.subplots(sub_x, sub_y)
+        axes = axes.flatten()
+        for j in range(y_values.shape[0]):  # j runs
+            axes[j].plot(t_values, y_values[j][i])
+            axes[j].set_title(f'Run {j + 1}')
+            axes[j].set_xlabel('t')
+            axes[j].grid(False)
+            axes[j].set_ylim(-1.2, 1.2)
+        fig.suptitle(f'Functional Factor {i + 1}', fontsize=16)
+        plt.tight_layout()
+        plt.show()
+
+
 class Basis:
     def evaluate_basis_function(self, i, t):
         raise NotImplementedError("The method evaluate_basis_function must be implemented by the subclass.")
